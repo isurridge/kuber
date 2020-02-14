64d11c67e99a65fd438788b7477e6d6bc5eb1c08
Ian <isurridge@gmail.com>
2020-02-14 13:48:25 -0800

    back to first main

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type Server struct{}
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
    w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "Hello World!"}`))
+   w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main() {
@@ -18,4 +18,3 @@ func main() {
    http.Handle("/", s)
    log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
